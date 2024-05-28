6b58f8cb86b62665b4b6c6f61441450b41d18f3f
bfloyd14 <brendanflo1425@gmail.com>
2024-05-28 17:44:57 -0400

    add index view& url, about view & url, add a Dog class in views.

--- finch-collector/main_app/urls.py
+++ finch-collector/main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('dogs/', views.dog_index, name='dog-index'),
 ]
--- finch-collector/main_app/views.py
+++ finch-collector/main_app/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
+# Add the Cat class & list and view function below the imports
+class Dog:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+old_dogs = [
+  Dog('Lola', 'tan with a hint of silver', 'Kinda sassy and is down for a good treat.', 7),
+  Dog('Sasha', 'blue merl', 'great nose and cuddler.', 10),
+  Dog('Moe', 'tan', 'short legs but nice teeth.', 9),
+  Dog('Joy', 'black and tan merl', 'Alpha, she just wants to play frisbee.', 11),
+  Dog('George', 'white', 'loves to lay in bed', 12)
+]
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def dog_index(request):
+  return render(request, {'old dogs': old_dogs})
