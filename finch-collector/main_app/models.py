b0f1295c7a7e5a448ebc1015b2d90921cc5d2036
bfloyd14 <brendanflo1425@gmail.com>
2024-05-30 12:22:36 -0400

    add Feeding model to models.py, add feeding form to forms.py and add feeding for to details template.

--- finch-collector/main_app/models.py
+++ finch-collector/main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('D', 'Dinner'),
+  ('T', 'Treat')
+)
+
 # Create your models here.
 class Dog(models.Model):
   name = models.CharField(max_length=100)
@@ -12,5 +18,18 @@ class Dog(models.Model):
     return self.name
   
   def get_absolute_url(self):
-      return reverse("dog-detail", kwargs={"dog_id": self.id})
+    return reverse("dog-detail", kwargs={"dog_id": self.id})
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices = MEALS,
+    default = MEALS[0][0]
+  ) 
+  
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.get_meal_display()} on {self.date}'
   
